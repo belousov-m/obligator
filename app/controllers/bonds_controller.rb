eff79b7e46ce3c8e093d21ae61ee6749150e8a24
Lena <derlaster@yandex.ru>
2022-09-06 17:33:52 +0300

    fix

--- app/controllers/bonds_controller.rb
+++ app/controllers/bonds_controller.rb
@@ -1,5 +1,8 @@
 class BondsController < ApplicationController
   def index
-    @bonds = Bond.where(mat_date: (Date.current..Date.current.next_year(2))).order(coupon_date: :desc)
+    @bonds = Bond.where(
+      mat_date: (Date.current..Date.current.next_year(3)),
+      coupon_date: (Date.current..Date.current.next_year(3))
+    ).order(coupon_date: :asc)
   end
 end

88e1bec551ba2e674e6a34b4458952d0c6a54a8a
Belousov Maksim <derlaster@yandex.ru>
2022-09-13 19:05:32 +0300

    added page

--- app/controllers/bonds_controller.rb
+++ app/controllers/bonds_controller.rb
@@ -1,8 +1,19 @@
 class BondsController < ApplicationController
+  before_action :set_resource, only: %i[show]
+
   def index
     @bonds = Bond.where(
+      type_name: 'ОФЗ',
       mat_date: (Date.current..Date.current.next_year(3)),
       coupon_date: (Date.current..Date.current.next_year(3))
     ).order(coupon_date: :asc)
+  end
+
+  def show; end
+
+  private
+
+  def set_resource
+    @bond = Bond.find(params[:id])
   end
 end
